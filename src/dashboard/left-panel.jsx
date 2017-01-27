897232aa6f7ad418dd0873316af5ac949f2a4c66
Kuldeep Keshwar <kuldeepkeshwar@197NODMB17820.local>
2017-01-27 14:59:41 +0530

    retro:menu

--- src/dashboard/left-panel.jsx
+++ src/dashboard/left-panel.jsx
@@ -1,56 +1,76 @@
 import React from 'react';
+const menus=[{
+  label:'Account Opening',
+  open:true,
+  children:[{
+    label:'Account Opening Status',
+    path:'/dashboard/account-status'
+  },{
+    label:'KYC',
+    path:'/dashboard/account-status'
+  },{
+    label:'Account',
+    path:'/dashboard/account-status'
+  }]
+},{
+  label:'Account Opening',
+  open:false,
+  children:[{
+    label:'Account Opening Status',
+    path:'/dashboard/account-status'
+  },{
+    label:'KYC',
+    path:'/dashboard/account-status'
+  },{
+    label:'Account opening ',
+    path:'/dashboard/account-status'
+  },{
+    label:'Account',
+    path:'/dashboard/account-status'
+  }]
+},{
+  label:'Account Opening',
+  open:false,
+  children:[{
+    label:'Account Opening Status',
+    path:'/dashboard/account-status'
+  },{
+    label:'KYC',
+    path:'/dashboard/account-status'
+  },{
+    label:'Account opening ',
+    path:'/dashboard/account-status'
+  },{
+    label:'Account',
+    path:'/dashboard/account-status'
+  }]
+}];
+function renderSubMenu(menus){
+  return menus.map((menu,i)=>{
+    return <li key={i}><a href="{menu.path}">{menu.label}</a></li>
+  })
+}
+function renderMenu(menus){
+  return menus.map((menu,i)=>{
+      let className=(menu.open)?'open':'';
+      return (<li key={i} className={className}>
+        <a href="javascript:void(0);">{menu.label} <span className="dropdown-arrow"></span></a>
+        <ul>
+          {renderSubMenu(menu.children)}
+        </ul>
+      </li>)
+  });
+}
 export default class LeftPanel extends React.Component {
   constructor(){
       super();
   }  
   render() {
+    const _menus=renderMenu(menus);
     return (
       <aside className="left-menu">
       <ul className="side-bar">
-        <li className="open">
-          <a href="">Account related <span className="dropdown-arrow"></span></a>
-          <ul>
-            <li><a href="">Account opening status</a></li>
-            <li><a href="">Account </a></li>
-            <li><a href="">status</a></li>
-          </ul>
-        </li>
-
-        <li className="">
-          <a href="">Account related <span className="dropdown-arrow"></span></a>
-          <ul>
-            <li><a href="">Account opening status</a></li>
-            <li><a href="">Account </a></li>
-            <li><a href="">status</a></li>
-          </ul>
-        </li>
-
-        <li className="">
-          <a href="">Account related <span className="dropdown-arrow"></span></a>
-          <ul>
-            <li><a href="">Account opening status</a></li>
-            <li><a href="">Account </a></li>
-            <li><a href="">status</a></li>
-          </ul>
-        </li>
-
-        <li className="">
-          <a href="">Account related <span className="dropdown-arrow"></span></a>
-          <ul>
-            <li><a href="">Account opening status</a></li>
-            <li><a href="">Account </a></li>
-            <li><a href="">status</a></li>
-          </ul>
-        </li>
-
-        <li className="">
-          <a href="">Account related <span className="dropdown-arrow"></span></a>
-          <ul>
-            <li><a href="">Account opening status</a></li>
-            <li><a href="">Account </a></li>
-            <li><a href="">status</a></li>
-          </ul>
-        </li>
+        {_menus}
       </ul>
     </aside>
     )
